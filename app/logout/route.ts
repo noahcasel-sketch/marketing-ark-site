8f0364319f4bb7f0bd9a35f56eb6d8ef69ca365a
noahcasel-sketch <noahcasel@marketing-ark.com>
2025-11-05 15:21:20 -0700

    Update route.ts

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { createClient } from "@lib/supabaseServer";
+import { createClient } from "../../lib/supabaseServer";
 
 export async function POST() {
   const supabase = createClient();
